19f4a197ca9523c081b62d490907c8297286720d
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-09-23 19:49:34 +0400

    hw2step1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Message from "./components/Message.js";
+import React, { useEffect } from "react";
 
 function App() {
-  const mainPage = "main page";
-  const message = "this is a message";
+  const [state, setState] = React.useState(0);
+  const [input, setInput] = React.useState("");
+
+  useEffect(() => {
+    console.log("render");
+  }, []);
 
   return (
     <div className="App">
-      <h1>{mainPage}</h1>
-      <Message message={message} />
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <p>{state}</p>
+      <button onClick={() => setState(state + 1)}>+</button>
     </div>
   );
 }
