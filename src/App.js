642e77be24d28592eaa9777cd070c94c5edf7dd3
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-09-28 22:59:48 +0400

    homework2
    done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,80 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [state, setState] = React.useState(0);
-  const [input, setInput] = React.useState("");
+  const [messageList, setMessageList] = useState([]);
+  const [messageText, setMessageText] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setMessageList((prevState) => [
+      ...prevState,
+      {
+        id: generateKey,
+        author: author,
+        messageText: messageText,
+      },
+    ]);
+
+    console.log(messageList);
+  };
 
   useEffect(() => {
-    console.log("render");
-  }, []);
+    setTimeout(() => {
+      botAnswer();
+    }, 1000);
+  }, [messageList]);
+
+  function generateKey(arr) {
+    return arr.length ? arr[arr.length - 1].id + 1 : 0;
+  }
+
+  function botAnswer() {
+    const authorAnswer = messageList[messageList.length - 1];
+
+    if (authorAnswer && authorAnswer.author) {
+      setMessageList((prevState) => [
+        ...prevState,
+        {
+          id: generateKey(prevState),
+          messageText: `bot answer: ${authorAnswer.author} - message shipped`,
+        },
+      ]);
+    }
+  }
 
   return (
     <div className="App">
-      <input value={input} onChange={(event) => setInput(event.target.value)} />
-      <p>{state}</p>
-      <button onClick={() => setState(state + 1)}>+</button>
+      <form onSubmit={handleSubmit}>
+        <br />
+        <input
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+          placeholder="author"
+        />
+        <br />
+        <input
+          value={messageText}
+          onChange={(event) => setMessageText(event.target.value)}
+          placeholder="text"
+        />
+        <br />
+        <button type="submit">Add message</button>
+      </form>
+
+      {messageList.map((message) => {
+        return (
+          <div>
+            <p>
+              {message.author} {message.messageText}
+            </p>
+            <p></p>
+          </div>
+        );
+      })}
     </div>
   );
 }
