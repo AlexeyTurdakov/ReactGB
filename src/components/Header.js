c8d7a37e208e242e8e0248eea1b7ed010c254aa1
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-10-07 21:55:17 +0400

    header material ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { amber } from "@mui/material/colors";
+import AppDrawer from "./Drawer.js";
 
-export default function Header(props) {
-  return <header>{props.header}</header>;
+export default class Header extends Component {
+  state = {
+    checkDrawer: false,
+  };
+
+  onChangeDrawer = () => {
+    this.setState({ checkDrawer: !this.state.checkDrawer });
+  };
+  render() {
+    return (
+      <header>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static" style={{ backgroundColor: amber[800] }}>
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                onClick={this.onChangeDrawer}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Chat-BotHelper
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+          <AppDrawer flag={this.state.checkDrawer} />
+        </Box>
+      </header>
+    );
+  }
 }
